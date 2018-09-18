bb546893293a27391ce3eaae1afc7da65d3681f5
Abdulaziz Saud <azoz-saud@live.com>
2018-09-19 02:07:02 +0300

    finished the basic login UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Colors.green,
       ),
       debugShowCheckedModeBanner: false,
       home: new MyHomePage(),
@@ -15,35 +15,35 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            child: Stack( //stack allow us to provide white space between two widgets
+            child: Stack(
+              //stack allow us to provide white space between two widgets
               children: <Widget>[
                 Container(
                   padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                  child: Text(
-                      'Welcome to',
+                  child: Text('Welcome to',
                       style: TextStyle(
-                          fontSize: 50.0, fontWeight: FontWeight.bold)
-                  ),
+                          fontSize: 50.0, fontWeight: FontWeight.bold)),
                 ),
                 Container(
                   padding: EdgeInsets.fromLTRB(15.0, 160.0, 0.0, 0.0),
                   child: Text(
                     'STUHousing',
-                    style: TextStyle(
-                        fontSize: 50.0, fontWeight: FontWeight.bold),
+                    style:
+                        TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
@@ -51,8 +51,9 @@ class _MyHomePageState extends State<MyHomePage>{
                   child: Text(
                     '.',
                     style: TextStyle(
-                        fontSize: 110.0, fontWeight: FontWeight.bold, color: Colors.lightGreen
-                    ),
+                        fontSize: 110.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green),
                   ),
                 ),
               ],
@@ -69,8 +70,63 @@ class _MyHomePageState extends State<MyHomePage>{
                           fontSize: 25.0,
                           fontFamily: 'Montserrat',
                           fontWeight: FontWeight.bold,
-                          color: Colors.black54
-                      )
+                          color: Colors.black54)),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 5.0),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontSize: 25.0,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54)),
+                  obscureText: true,
+                ),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        decoration: TextDecoration.underline
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50.0),
+                Container(
+                  height: 50.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.greenAccent,
+                    color: Colors.green,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Monteserrat',
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
