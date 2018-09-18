97253836fd2802d946973a375e74180ed46a2ce4
Kashgary <ab70x9@gmail.com>
2018-09-18 21:43:50 +0300

    Orgnize The Project in classes and folders

---
+++ lib/screen/Home/MyHomePage.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget{
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Stack( //stack allow us to provide white space between two widgets
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                      'Welcome to',
+                      style: TextStyle(
+                          fontSize: 50.0, fontWeight: FontWeight.bold)
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 160.0, 0.0, 0.0),
+                  child: Text(
+                    'STUHousing',
+                    style: TextStyle(
+                        fontSize: 50.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(304.0, 115.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                        fontSize: 110.0, fontWeight: FontWeight.bold, color: Colors.lightGreen
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'User Email',
+                      labelStyle: TextStyle(
+                          fontSize: 25.0,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54
+                      )
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
