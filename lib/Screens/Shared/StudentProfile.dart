2d9b5227a807416e2a68aa8700fc4dac6a78d4e9
Abdulaziz Saud <azoz-saud@live.com>
2018-11-18 21:48:48 +0300

    View room , student profile

---
+++ lib/Screens/Shared/StudentProfile.dart
@@ -0,0 +1,306 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class StudentProfile extends StatefulWidget {
+  @override
+  StudentProfileState createState() => new StudentProfileState();
+
+  final String uid;
+
+  //constructor
+  StudentProfile({
+    this.uid,
+  });
+}
+
+class StudentProfileState extends State<StudentProfile> {
+  String name;
+  String email;
+  String city;
+  String mobile;
+  String intrestsHobbies;
+  String dislike;
+  String graduationTerm;
+  String major;
+  String smoking;
+  bool bol = false;
+
+  @override
+  void initState() {
+    FirebaseAuth.instance.currentUser().then((FirebaseUser user) async {
+      await Firestore.instance
+          .collection('Users')
+          .document(widget.uid)
+          .get()
+          .then((data) {
+        if (data.exists) {
+          setState(() {
+            this.name = data['Name'];
+            this.email = data['Email'];
+            this.major = data['Major'];
+            this.city = data['City'];
+            this.graduationTerm = data['GraduationTerm'];
+            this.smoking = data['Smoking'];
+            this.mobile = data['Mobile'];
+            this.intrestsHobbies = data['IntrestsHobbies'];
+            this.dislike = data['Dislikes'];
+            this.bol = true;
+          });
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          'Profile',
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: new FutureBuilder<DocumentSnapshot>(
+          future: Firestore.instance.collection('Users').document(widget.uid).get(),
+          builder: (context, snapshot) {
+            if (!bol) {
+              return new Center(
+                child: new CircularProgressIndicator(),
+              );
+            }
+            return new ListView(children: <Widget>[
+              new Stack(
+                children: <Widget>[
+                  Positioned(
+                    width: 350.0,
+                    left: 10.0,
+                    top: MediaQuery.of(context).size.height / 40,
+                    child: Column(
+                      children: <Widget>[
+                        new Center(
+                          child: Container(
+                            width: 150.0,
+                            height: 150.0,
+                            decoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        'https://i.stack.imgur.com/l60Hf.png'),
+                                    fit: BoxFit.cover),
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(75.0)),
+                                boxShadow: [
+                                  BoxShadow(
+                                      blurRadius: 20.0, color: Colors.black)
+                                ]),
+                          ),
+                        ),
+                        SizedBox(height: 35.0),
+                        new Text(
+                          '$name',
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: new TextStyle(
+                              fontSize: 25.0, fontWeight: FontWeight.bold),
+                        ),
+                        FlatButton(
+                          onPressed: _launchEmail,
+                          child: Text(
+                            '$email',
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            style: new TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 25.0),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 310.0,
+                    child: Container(
+                      color: Colors.black.withOpacity(0.58),
+                      margin: const EdgeInsets.symmetric(vertical: 16.0),
+                      width: 225.0,
+                      height: 2.0,
+                    ),
+                  ),
+                  Positioned(
+                    top: 300.0,
+                    child: Container(
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.only(left: 5.0),
+                            child: Text(
+                              'Information',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24.0,
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 326.0,
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(left: 5.0),
+                          child: Stack(
+                            children: <Widget>[
+
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                      padding: EdgeInsets.only(top: 60.0)),
+                                  Text(
+                                    'City:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16.0),
+                                  ),
+                                  Text(
+                                    '  $city',
+                                    style: TextStyle(fontSize: 16.0),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                      padding: EdgeInsets.only(top: 105.0)),
+                                  Text(
+                                    'Graduation Term:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16.0),
+                                  ),
+                                  Text(
+                                    '  $graduationTerm',
+                                    style: TextStyle(fontSize: 16.0),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                      padding: EdgeInsets.only(top: 150.0)),
+                                  Text(
+                                    'Smoking:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16.0),
+                                  ),
+                                  Text(
+                                    '  $smoking',
+                                    style: TextStyle(fontSize: 16.0),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                      padding: EdgeInsets.only(top: 200.0)),
+                                  Text(
+                                    'Mobile Number:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16.0),
+                                  ),
+                                  Text(
+                                    '  $mobile',
+                                    style: TextStyle(fontSize: 16.0),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                      padding: EdgeInsets.only(top: 240.0)),
+                                  Text(
+                                    'Major:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16.0),
+                                  ),
+                                  Text(
+                                    '  $major',
+                                    style: TextStyle(fontSize: 16.0),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    child: Container(
+                      padding: EdgeInsets.only(top: 470.0),
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: <Widget>[
+                          ExpansionTile(
+                            title: Text(
+                              'Intrests & Hobbies',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            children: <Widget>[
+                              Text('$intrestsHobbies'),
+                            ],
+                          ),
+                          ExpansionTile(
+                            title: Text(
+                              'Things I dislike',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            children: <Widget>[
+                              Text('$dislike'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ]);
+          }),
+    );
+  }
+
+  _launchEmail() async {
+    String url = 'mailto:' + email + '?subject=News&body=New%20plugin';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
+
--- lib/Screens/Shared/ViewRoom.dart
+++ lib/Screens/Shared/ViewRoom.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'StudentProfile.dart';
 
 class ViewRoom extends StatefulWidget {
   @override
   ViewRoomState createState() => new ViewRoomState();
   final String buildingNumber;
   final String roomNumber;
+
   //constructor
   ViewRoom({
     this.buildingNumber,
@@ -15,6 +17,37 @@ class ViewRoom extends StatefulWidget {
 }
 
 class ViewRoomState extends State<ViewRoom> {
+  QuerySnapshot doc;
+  String uid1;
+  String uid2;
+  String stuEmail1;
+  String stuEmail2;
+  String roomStatus;
+  var stream;
+  bool bol = false;
+
+  @override
+  void initState() {
+    FirebaseAuth.instance.currentUser().then((FirebaseUser user) async {
+      await Firestore.instance
+          .collection('Room')
+          .document('${widget.buildingNumber}-${widget.roomNumber}')
+          .get()
+          .then((data) {
+        if (data.exists) {
+          setState(() {
+            this.uid1 = data['UID1'];
+            this.uid2 = data['UID2'];
+            this.stuEmail1 = data['Email1'];
+            this.stuEmail2 = data['Email2'];
+            this.roomStatus = data['room_status'];
+            this.bol = true;
+          });
+        }
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +56,116 @@ class ViewRoomState extends State<ViewRoom> {
         title: new Text('B${widget.buildingNumber} R${widget.roomNumber}'),
         centerTitle: true,
       ),
-      body: Container(
-
-      ),
+      body: new FutureBuilder<DocumentSnapshot>(
+          future: Firestore.instance
+              .collection('Room')
+              .document('${widget.buildingNumber}-${widget.roomNumber}')
+              .get(),
+          builder: (context, snapshot) {
+            if (!bol) {
+              return new Center(
+                child: new CircularProgressIndicator(),
+              );
+            }
+            return ListView(
+              children: <Widget>[
+                SizedBox(height: 15.0,),
+                Row(
+                  children: <Widget>[
+                    Text('Room number: ',
+                      style: new TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('${widget.roomNumber}'),
+                  ],
+                ),
+                SizedBox(height: 15.0,),
+                Row(
+                  children: <Widget>[
+                    Text('Building number: ',
+                      style: new TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('${widget.buildingNumber}'),
+                  ],
+                ),
+                SizedBox(height: 15.0,),
+                Row(
+                  children: <Widget>[
+                    Text('Room status: ',
+                      style: new TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('$roomStatus'),
+                  ],
+                ),
+                SizedBox(height: 15.0,),
+                Text(
+                  'Room occupiers',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Student1: '),
+                    FlatButton(
+                      onPressed:(){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => StudentProfile(
+                              uid: uid1,
+                            ),
+                          ),
+                        );                      },
+                      child: Text(
+                        '$stuEmail1',
+                        overflow: TextOverflow.ellipsis,
+                        style: new TextStyle(
+                          color: Colors.black54,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Student2: '),
+                    FlatButton(
+                      onPressed:(){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => StudentProfile(
+                              uid: uid2,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        '$stuEmail2',
+                        overflow: TextOverflow.ellipsis,
+                        style: new TextStyle(
+                          color: Colors.black54,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          }),
     );
   }
 }
