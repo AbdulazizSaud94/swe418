a2646906141100fe1796da0c35a520852a86cd77
Abdulaziz Saud <azoz-saud@live.com>
2018-11-18 21:48:30 +0300

    View room, student profile

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ import 'Screens/Student/change_room_request.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'Screens/Shared/ViewBuilding.dart';
 import 'Screens/Shared/ViewRoom.dart';
+import 'Screens/Shared/StudentProfile.dart';
 
 void main() {
   MapView.setApiKey('AIzaSyCKMhiABoRdSTWZ15iwRkhqCwJtShqQZGQ');
@@ -91,6 +92,7 @@ class MyApp extends StatelessWidget {
         '/ChangeRoomPage': (BuildContext context) => new ChangeRoomPage(),
         '/ViewBuilding':  (BuildContext context) => new ViewBuilding(),
         '/ViewRoom': (BuildContext context) => new ViewRoom(),
+        '/StudentProfile': (BuildContext context) => new StudentProfile(),
       },
     );
   }
