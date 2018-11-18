0f2ffc4142c9cf6e66a50e98544c96a89e23741c
Abdulaziz Saud <azoz-saud@live.com>
2018-11-18 22:18:39 +0300

    added swap page

--- lib/Screens/Shared/StudentProfile.dart
+++ lib/Screens/Shared/StudentProfile.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 class StudentProfile extends StatefulWidget {
---
+++ lib/Screens/Student/RequestSwap.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RequestSwap extends StatefulWidget {
+  @override
+  RequestSwapState createState() => new RequestSwapState();
+}
+
+class RequestSwapState extends State<RequestSwap> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
